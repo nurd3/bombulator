edc040919475950347925d546fec14f1795de4ca
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-16 16:47:05 -0500

    fix cheating.lua (2) (sigh)

--- cheating.lua
+++ cheating.lua
@@ -4,43 +4,43 @@ end
 
 local function punish_moved_too_fast(player)
     local playername = player:get_player_name()
-    bomulator.chat_send_all("%s moved too fast !!! >:(", playername)
+    bombulator.chat_send_all("%s moved too fast !!! >:(", playername)
 
 end
 
 local function punish_interacted_too_far(player)
     local playername = player:get_player_name()
-    bomulator.chat_send_all("%s interacted too far !!! >:(", playername)
+    bombulator.chat_send_all("%s interacted too far !!! >:(", playername)
 
 end
 
 local function punish_interacted_with_self(player)
     local playername = player:get_player_name()
-    bomulator.chat_send_all("%s interacted too far !!! >:(", playername)
+    bombulator.chat_send_all("%s interacted too far !!! >:(", playername)
 
 end
 
 local function punish_interacted_while_dead(player)
     local playername = player:get_player_name()
-    bomulator.chat_send_all("%s interacted while dead !!! >:(", playername)
+    bombulator.chat_send_all("%s interacted while dead !!! >:(", playername)
 
 end
 
 local function punish_finished_unknown_dig(player)
     local playername = player:get_player_name()
-    bomulator.chat_send_all("%s finished unknown dig !!! >:(", playername)
+    bombulator.chat_send_all("%s finished unknown dig !!! >:(", playername)
 
 end
 
 local function punish_dug_unbreakable(player)
     local playername = player:get_player_name()
-    bomulator.chat_send_all("%s dug unbreakable !!! >:(", playername)
+    bombulator.chat_send_all("%s dug unbreakable !!! >:(", playername)
 
 end
 
 local function punish_dug_too_fast(player)
     local playername = player:get_player_name()
-    bomulator.chat_send_all("%s dug too fast !!! >:(", playername)
+    bombulator.chat_send_all("%s dug too fast !!! >:(", playername)
 
 end
 
