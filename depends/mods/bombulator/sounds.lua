acc8f828e2b8e73bdcc9ec5d5bff736ff6b3638c
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-16 17:12:15 -0500

    node sounds

--- depends/mods/bombulator/sounds.lua
+++ depends/mods/bombulator/sounds.lua
@@ -40,6 +40,40 @@ bombulator.register_bombulation("bombulator:spatial_sound", {
     end
 })
 
+local node_sound_range = 64.0
+
+local function random_key(tbl)
+    local temp = {}
+
+    for k, v in pairs(tbl) do
+        table.insert(temp, k)
+    end
+
+    return temp[math.random(#temp)]
+end
+
+function bombulator.node_sound(origin, playername)
+    for _ = 1, node_sound_range * 4 do
+        local pos = vector.round(origin + vector.random_direction() * math.random() * node_sound_range)
+        local node_name = core.get_node(pos).name
+        local sounds = core.registered_nodes[node_name] and core.registered_nodes[node_name].sounds
+
+        if sounds then
+            core.sound_play(random_key(sounds), {
+                to_player = playername,
+                pos = pos
+            })
+        end   
+    end
+end
+
+bombulator.register_bombulation("bombulator:node_sound", {
+    interval = 15.0,
+    per_player = function(player)
+        bombulator.node_sound(player:get_pos(), player:get_player_name())
+    end
+})
+
 bombulator.register_sounds {
     ["bombulator_fart"] = { chance = 0.005 },
     ["bombulator_bombulator_name"] = { chance = 0.005 },
