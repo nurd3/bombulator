1f8aeaeca147a2abef11616dd7dff97e714a58e1
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-16 15:32:56 -0500

    primitive exile support

---
+++ depends/mods/nodes_nature/init.lua
@@ -0,0 +1,33 @@
+bombulator.register_nodes {
+    ["nodes_nature:sand"] = { chance = 1.0 },
+    ["nodes_nature:silt"] = { chance = 1.0 },
+    ["nodes_nature:clay"] = { chance = 1.0 },
+    ["nodes_nature:gravel"] = { chance = 1.0 },
+    ["nodes_nature:loam"] = { chance = 1.0 },
+    ["nodes_nature:volcanic_ash"] = { chance = 1.0 },
+
+    ["nodes_nature:woodland_soil"] = { chance = 1.0 },
+    ["nodes_nature:marshland_soil"] = { chance = 1.0 },
+    ["nodes_nature:grassland_soil"] = { chance = 1.0 },
+    ["nodes_nature:duneland_soil"] = { chance = 1.0 },
+    ["nodes_nature:highland_soil"] = { chance = 1.0 },
+    ["nodes_nature:grassland_barren_soil"] = { chance = 1.0 },
+    ["nodes_nature:woodland_dry_soil"] = { chance = 1.0 },
+
+    ["nodes_nature:sandstone"] = { chance = 1.0 },
+    ["nodes_nature:siltstone"] = { chance = 1.0 },
+    ["nodes_nature:claystone"] = { chance = 1.0 },
+    ["nodes_nature:conglomerate"] = { chance = 1.0 },
+
+    ["nodes_nature:limestone"] = { chance = 1.0 },
+    ["nodes_nature:ironstone"] = { chance = 1.0 },
+    ["nodes_nature:granite"] = { chance = 1.0 },
+    ["nodes_nature:basalt"] = { chance = 1.0 },
+    ["nodes_nature:gneiss"] = { chance = 1.0 },
+    ["nodes_nature:jade"] = { chance = 1.0 },
+
+    ["nodes_nature:maraka_tree"] = { chance = 1.0 },
+    ["nodes_nature:tangkal_tree"] = { chance = 1.0 },
+    ["nodes_nature:sasaran_tree"] = { chance = 1.0 },
+    ["nodes_nature:kagum_tree"] = { chance = 1.0 },
+}
