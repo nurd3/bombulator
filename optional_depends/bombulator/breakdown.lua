127907c3262a04ac4162b1b9f7b37d30df42455f
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-16 04:13:57 -0500

    breakdown bombulation

---
+++ optional_depends/bombulator/breakdown.lua
@@ -0,0 +1,31 @@
+local breakdown_range = 20
+
+function bombulator.breakdown(origin, speed_mult_min, speed_mult_max)
+
+    for _ = 1, breakdown_range * 2 do
+        local pos = vector.round(origin + vector.random_direction() * math.random() * breakdown_range)
+        local node = core.get_node(pos)
+        local speed_mult = math.random(speed_mult_min, speed_mult_max)
+
+        if node.name == "air" then
+            node.name = bombulator.random_node()
+        end
+
+        if not node.name then return end
+
+        if bombulator.registered_bombulations[node.name] then
+            bombulator.add_fake_node(pos, speed_mult, node)
+        end
+    end
+
+end
+
+
+bombulator.register_bombulation("bombulator:breakdown", {
+    interval = 300,
+    per_player = function(player)
+        local speed_mult = player:get_physics_override().speed * player:get_physics_override().speed_walk
+
+        bombulator.breakdown(player:get_pos(), 0.5, speed_mult)
+    end
+})
--- optional_depends/bombulator/init.lua
+++ optional_depends/bombulator/init.lua
@@ -2,3 +2,4 @@ local path = optional_depends.current_path
 
 dofile (path .. "/sounds.lua")
 dofile (path .. "/scaredodes.lua")
+dofile (path .. "/breakdown.lua")
