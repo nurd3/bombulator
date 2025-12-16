e400ffae9d5992b81c03fba00f81406fbe2637ac
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-16 04:21:58 -0500

    traps

--- optional_depends/bombulator/init.lua
+++ optional_depends/bombulator/init.lua
@@ -2,4 +2,5 @@ local path = optional_depends.current_path
 
 dofile (path .. "/sounds.lua")
 dofile (path .. "/scaredodes.lua")
+dofile (path .. "/traps.lua")
 dofile (path .. "/breakdown.lua")
---
+++ optional_depends/bombulator/traps.lua
@@ -0,0 +1,54 @@
+local trap_range = 128.0
+
+function bombulator.big_trap(origin, speed_mult_min, speed_mult_max, velocity)
+    local cube_centre = origin + velocity * trap_range
+    local _, ray_hit = core.line_of_sight(origin, cube_centre)
+
+    cube_centre = ray_hit or cube_centre
+
+    core.log("info", "bombulator.big_trap")
+
+    for x = -1, 1 do for y = -1, 1 do for z = -1, 1 do
+        local pos = vector.round(vector.offset(cube_centre, x, y, z))
+        local node = core.get_node(pos)
+        local speed_mult = math.random(speed_mult_min, speed_mult_max)
+
+        if node.name and bombulator.registered_nodes[node.name] then
+            bombulator.add_fake_node(pos, speed_mult, node)
+        end
+    end end end
+
+end
+
+bombulator.register_bombulation("bombulator:big_trap", {
+    interval = 150.0,
+    per_player = function(player)
+        bombulator.big_trap(player:get_pos(), 0.5, 2.0, player:get_velocity())
+    end
+})
+
+function bombulator.small_trap(origin, speed_mult_min, speed_mult_max, velocity)
+    local cube_centre = origin + velocity * trap_range
+    local _, ray_hit = core.line_of_sight(origin, cube_centre)
+
+    cube_centre = ray_hit or cube_centre
+
+    core.log("info", "bombulator.small_trap")
+
+    local pos = vector.round(cube_centre)
+    local node = core.get_node(pos)
+    local speed_mult = math.random(speed_mult_min, speed_mult_max)
+
+    if node.name and bombulator.registered_nodes[node.name] then
+        bombulator.add_fake_node(pos, speed_mult, node)
+    end
+
+end
+
+
+bombulator.register_bombulation("bombulator:small_trap", {
+    interval = 60.0,
+    per_player = function(player)
+        bombulator.small_trap(player:get_pos(), 0.5, 2.0, player:get_velocity())
+    end
+})
