77ac145326190581ed71b2eafdd2fb6f4211edd2
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-16 17:12:37 -0500

    decrease thread use

--- bombulation.lua
+++ bombulation.lua
@@ -3,27 +3,26 @@ local global_memory = {}
 local default_interval = 5
 local default_func = function() end
 
-local function bombulation_loop(bombuname, playername)
+local function bombulation_loop(bombuname)
     core.log("info", "bombulation_loop()")
 
     local def = bombulator.registered_bombulations[bombuname]
     local interval = def.interval or default_interval
-    local inverse_interval = 0.125 / interval
+    local inverse_interval = 0.25 / interval
     local func = def.per_player or default_func
     local local_memory = {}
 
     local timeout = function(self)
         core.log("info", "timeout()")
         
-        if math.random() < inverse_interval then
-            local player = core.get_player_by_name(playername)
-
-            if not player then return end
-
-            func(player, local_memory, global_memory[bombuname])
+        
+        for _, playername in ipairs(core.get_connected_players()) do
+            if math.random() < inverse_interval then
+                func(core.get_player_by_name(playername), local_memory, global_memory[bombuname])
+            end
         end
 
-        core.after(0.125, self, self)
+        core.after(0.25, self, self)
     end
 
 
@@ -38,12 +37,12 @@ function bombulator.begin()
     end
 end
 
-local function joinplayer(player)
-    core.log("info", "joinplayer()")
+local function joinplayer()
+    core.log("info", "mods_loaded()")
     for name, _ in pairs(bombulator.registered_bombulations) do
         core.log("info", name)
-        bombulation_loop(name, player:get_player_name())
+        bombulation_loop(name)
     end
 end
 
-core.register_on_joinplayer(joinplayer)
+core.register_on_mods_loaded(mods_loaded)
