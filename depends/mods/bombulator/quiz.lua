1e2249f376a086c51ca824cae27ff2741a811175
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-17 16:52:46 -0500

    quiz update

--- depends/mods/bombulator/quiz.lua
+++ depends/mods/bombulator/quiz.lua
@@ -15,74 +15,145 @@ local function arithmetic()
     elseif operator == "-" then answer = lhs - rhs end
     
     return {
-        question = fmt("%i %s %i = ???", lhs, operator, rhs),
-        answers = {answer}
+        question = fmt("label[0.25,0.5;%i %s %i = ???]", lhs, operator, rhs),
+        answer = tostring(answer),
+        options = {
+            answer + random(arithmetic_min, arithmetic_max) + 1,
+            answer - random(arithmetic_min, arithmetic_max) - 1,
+            answer + random(arithmetic_min, arithmetic_max) + 1,
+            answer - random(arithmetic_min, arithmetic_max) - 1
+        }
     }
 end
 
 local trivia_questions = {
     {
-        question = S"What's my password?",
-        answers = {
-            "123456",
-            "password",
-            "qwerty",
-            "1234"
-        }
-    },
-    {
-        question = "OwO?",
-        answers = {
-            "owo",
-            "uwu",
-            "qwq",
-            "-w-"
+        question = S"What's the password?",
+        answer = "password",
+        options = {
+            "M49m7,,['N1n",
+            "7Xog_i085$Xx",
+            "I/Inw{41-6+?",
+            "V5m2!1`V-h+I",
+            "2TkYG2\"O\\y9$",
+            "+@1[Wu57MdTV",
+            "46A)a-82b~Z)",
+            "U9w>YI9k#4+h",
         }
     },
     {
         question = S"When did the 21st century begin?",
-        answers = {
-            "2000",
-            "'00",
-            "00"
+        answer = "2000",
+        options = {
+            "1900",
+            "2100"
         }
     },
     {
-        question = S"When did the year 1999 start?",
-        answers = {
-            "99",
-            "'99",
-            "1999",
-            "january 1st",
-            "1st january",
-            "1st of january",
-            "01/01",
+        question = S"What day started the year 1999?",
+        answer = S"January 1st",
+        options = {
+            S"1999",
+            S"'99",
+            S"Februralarry 29nd"
         }
-    }
+    },
+    {
+        question = S"When did the Mayan calendar end?",
+        answer = S"2012 December 21st",
+        options = {
+            S"1927 April 12th",
+            S"1967 June 7th",
+            S"1966 October 15th",
+            S"1917 November 7th",
+            S"1848 February 21st",
+            S"3013 Septuary 42th",
+            S"3001 Marchember 39nd"
+        }
+    },
+    {
+        question = S"Say my name.",
+        answer = S"B O M B U L A T O R",
+        options = {
+            S"Minetest",
+            S"NodeCore",
+            S"Unlit",
+            S"Void - An empty game",
+            S"Age of Mending"
+        }
+    },
 }
 
 local function trivia()
     table.shuffle(trivia_questions)
-    return trivia_questions[1]
+    local quiz = table.copy(trivia_questions[1])
+    quiz.question = fmt("label[0.25,0.5;%s]", core.formspec_escape(quiz.question))
+    return quiz
 end
 
-local quiz_answers = {}
+local function guess_the_node()
+    local node_name = bombulator.random_node()
+
+    if not node_name then return end
+
+    local options = {}
+
+    for _ = 1, 2 do
+        local option = bombulator.random_node()
+        while option == node_name do option = bombulator.random_node() end
+        table.insert(options, option)
+    end
+
+    return {
+        question = fmt([[
+            label[0.25,0.5;%s]
+            item_image[0.25,0.75;1.0,1.0;%s]
+        ]], core.formspec_escape(S"What is this node?"), node_name),
+        answer = node_name,
+        options = options
+    }
+end
+
+local function coinflip()
+    local answer = "tails"
+
+    if random(2) == 1 then answer = "heads" end
+
+    return {
+        question = "label[0.25,0.5;heads or tails?]",
+        answer = answer,
+        options = { "neither", "both" }
+    }
+end
+
+local ongoing_quizzes = {}
 
 function bombulator.show_quiz_formspec(playername, quiz)
-    quiz_answers[playername] = quiz.answers
+    ongoing_quizzes[playername] = quiz
+
+    local options = quiz.options
+
+    -- shuffle options and make sure there's only 3 options
+    table.shuffle(options)
+    options = {options[1], options[2], quiz.answer}
+    table.shuffle(options)
 
     core.show_formspec(playername, "bombulator:quiz", [[
         formspec_version[4]
-        size[8,4]
+        size[8,8]
         position[0.5,0.5]
         allow_close[false]
         no_prepend[]
-        field[0.25,2.25;4.0,0.5;response;Your Answer;]
-        button[0.25,3.0;4.0,0.5;submit;Submit]
-    ]] .. fmt("label[0.25,0.5;%s]", core.formspec_escape(quiz.question)))
+    ]]
+    .. fmt([[
+        button[0.25,3.0;4.0,0.5;response;%s]
+        button[0.25,3.5;4.0,0.5;response;%s]
+        button[0.25,4.0;4.0,0.5;response;%s]
+    ]], options[1], options[2], options[3])
+    .. quiz.question)
 end
 
-local quiz_generators = {arithmetic, trivia}
+local quiz_generators = {arithmetic, trivia, coinflip, guess_the_node}
 
 function bombulator.random_quiz()
     table.shuffle(quiz_generators)
@@ -95,28 +166,25 @@ function bombulator.give_quiz_to_player(player)
     local playername = player:get_player_name()
     local quiz = bombulator.random_quiz()
 
-    if quiz_answers[playername] then return end
+    while not quiz do bombulator.random_quiz() end
+
+    -- do not show player another quiz if they already have a quiz
+    if ongoing_quizzes[playername] then return end
 
     bombulator.show_quiz_formspec(playername, quiz) 
 end
 
 bombulator.register_bombulation("bombulator:quiz", {
-    interval = 120.0,
+    interval = 60.0,
     per_player = function(player)
         bombulator.give_quiz_to_player(player)
     end
 })
 
 local function is_answer(playername, response)
-    if not response or not playername or not quiz_answers[playername] then return end
-
-    local answers = quiz_answers[playername]
-
-    for _, answer in ipairs(answers) do
-        if tostring(answer) == tostring(response) then return true end
-    end
-
-    return false
+    if not response or not playername or not ongoing_quizzes[playername] then return end
+    
+    return tostring(response) == tostring(ongoing_quizzes[playername].answer)
 end
 
 core.register_on_player_receive_fields(function(player, formname, fields)
@@ -128,10 +196,10 @@ core.register_on_player_receive_fields(function(player, formname, fields)
     if is_answer(playername, response) then
         core.chat_send_player(playername, core.colorize("#00ff00", S"Correct answer! You get to keep playing!"))
         core.close_formspec(playername, "bombulator:quiz")
-        quiz_answers[playername] = nil
+        ongoing_quizzes[playername] = nil
     else
         core.chat_send_player(playername, core.colorize("#ff0000", S"Incorrect answer! Try again!"))
-        quiz_answers[playername] = nil
+        ongoing_quizzes[playername] = nil
         bombulator.give_quiz_to_player(player)
     end
 end)
