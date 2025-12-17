94d16220449ec23af13b3c6dcf28612772146d74
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-17 09:04:09 -0500

    random boost to jumping mechanic

--- depends/mods/bombulator/init.lua
+++ depends/mods/bombulator/init.lua
@@ -1,6 +1,7 @@
 local path = deploader.current_path
 
 dofile (path .. "/quiz.lua")
+dofile (path .. "/leap.lua")
 dofile (path .. "/sounds.lua")
 dofile (path .. "/scaredodes.lua")
 dofile (path .. "/traps.lua")
---
+++ depends/mods/bombulator/leap.lua
@@ -0,0 +1,12 @@
+function bombulator.leap(player)
+    local controls = player:get_player_control()
+
+    if controls.jump then player:add_velocity(vector.new(0, 10, 0)) end
+end
+
+bombulator.register_bombulation("bombulator:leap", {
+    interval = 60.0,
+    per_player = function(player)
+        bombulator.leap(player)
+    end
+})
