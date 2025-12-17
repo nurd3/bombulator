e62dab29c4b816e6dfbf955754652320abbaf934
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-17 17:21:50 -0500

    fix some typos

--- depends/mods/nc_terrain/init.lua
+++ depends/mods/nc_terrain/init.lua
@@ -4,5 +4,6 @@ bombulator.register_nodes {
     ["nc_terrain:gravel"] = { chance = 1.0 },
     ["nc_terrain:sand"] = { chance = 1.0 },
     ["nc_terrain:stone"] = { chance = 1.0 },
-    ["nc_terrain:hard_stone"] = { chance = 1.0 },
+    ["nc_terrain:hard_stone_1"] = { chance = 1.0 },
+    ["nc_terrain:hard_stone_2"] = { chance = 1.0 },
 }
