46172033cb6e0a9b74ded945740940fb2801499e
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-17 21:36:05 -0500

    support for mobs_monster

---
+++ depends/mods/mobs_monster/init.lua
@@ -0,0 +1,12 @@
+bombulator.register_entities {
+    ["mobs_monster:dirt_monster"] = { chance = 1.0 },
+    ["mobs_monster:dungeon_master"] = { chance = 1.0 },
+    ["mobs_monster:fire_spirit"] = { chance = 1.0 },
+    ["mobs_monster:land_guard"] = { chance = 1.0 },
+    ["mobs_monster:lava_flan"] = { chance = 1.0 },
+    ["mobs_monster:oerkki"] = { chance = 1.0 },
+    ["mobs_monster:sand_monster"] = { chance = 1.0 },
+    ["mobs_monster:spider"] = { chance = 1.0 },
+    ["mobs_monster:stone_monster"] = { chance = 1.0 },
+    ["mobs_monster:tree_monster"] = { chance = 1.0 },
+}
