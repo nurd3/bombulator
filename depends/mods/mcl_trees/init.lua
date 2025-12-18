aa8fcf8ce46287617dbdae146a921c9a13a047b7
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-18 07:12:19 -0500

    mineclonia support

---
+++ depends/mods/mcl_trees/init.lua
@@ -0,0 +1,8 @@
+bombulator.register_nodes {
+    ["mcl_trees:tree_oak"] = { chance = 1.0 },
+    ["mcl_trees:tree_dark_oak"] = { chance = 1.0 },
+    ["mcl_trees:tree_jungle"] = { chance = 1.0 },
+    ["mcl_trees:tree_spruce"] = { chance = 1.0 },
+    ["mcl_trees:tree_acacia"] = { chance = 1.0 },
+    ["mcl_trees:tree_birch"] = { chance = 1.0 }
+}
