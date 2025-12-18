5b72e3bfd0257ba3c72743481bd80ff8c22b5477
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-18 06:16:22 -0500

    make fake builtin player remove itself on activate

--- depends/mods/bombulator/entities.lua
+++ depends/mods/bombulator/entities.lua
@@ -2,7 +2,8 @@ core.register_entity("bombulator:builtin_player", {
     visual = "upright_sprite",
     collisionbox = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
     visual_size = {x = 1, y = 2},
-    textures = {"player.png", "player_back.png"}
+    textures = {"player.png", "player_back.png"},
+    on_activate = function(self) self.object:remove() end
 })
 
 bombulator.register_entities { ["bombulator:builtin_player"] = { chance = 1.0 } }
