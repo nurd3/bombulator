5e98bc596eb5bc06bd642aa52ce3dc902a9f7c4a
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-17 21:37:05 -0500

    uncomment code I forgot I commented out >:(

--- depends/mods/bombulator/ghost_types.lua
+++ depends/mods/bombulator/ghost_types.lua
@@ -1,29 +1,29 @@
--- local stander_disappear_range = 4.0
+local stander_disappear_range = 4.0
 
--- bombulator.ghost_types["bombulator:stander"] = {
---     chance = 1.0,
---     on_step = function(self, _, _, player)
---         local dist = vector.distance(self.object:get_pos(), player:get_pos())
---         if dist < stander_disappear_range then self.object:remove() return end
---     end
--- }
+bombulator.ghost_types["bombulator:stander"] = {
+    chance = 1.0,
+    on_step = function(self, _, _, player)
+        local dist = vector.distance(self.object:get_pos(), player:get_pos())
+        if dist < stander_disappear_range then self.object:remove() return end
+    end
+}
 
--- local chaser_speed = 8.0
--- local chaser_chase_range = 32.0
+local chaser_speed = 8.0
+local chaser_chase_range = 32.0
 
--- bombulator.ghost_types["bombulator:chaser"] = {
---     chance = 0.5,
---     on_step = function(self, dtime, _, player)
---         local pos = self.object:get_pos()
---         local dist = vector.distance(pos, player:get_pos())
+bombulator.ghost_types["bombulator:chaser"] = {
+    chance = 0.5,
+    on_step = function(self, dtime, _, player)
+        local pos = self.object:get_pos()
+        local dist = vector.distance(pos, player:get_pos())
         
---         if dist < stander_disappear_range then
---             self.object:remove() return
---         elseif dist < chaser_chase_range then
---             self.object:set_velocity(vector.direction(pos, player:get_pos()) * chaser_speed)
---         end
---     end
--- }
+        if dist < stander_disappear_range then
+            self.object:remove() return
+        elseif dist < chaser_chase_range then
+            self.object:set_velocity(vector.direction(pos, player:get_pos()) * chaser_speed)
+        end
+    end
+}
 
 local killer_speed = 8.0
 local killer_timer = 16.0
