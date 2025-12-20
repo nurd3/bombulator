8bebac004158b3ec72ffca574a34d7811550201b
nurd3 <50721698+nurd3@users.noreply.github.com.>
2025-12-20 09:52:09 -0500

    add river_water_source for void_essential

--- depends/mods/void_essential/init.lua
+++ depends/mods/void_essential/init.lua
@@ -1,4 +1,5 @@
 bombulator.register_nodes {
     ["void_essential:stone"] = { chance = 1.0 },
-    ["void_essential:water_source"] = { chance = 1.0 }
+    ["void_essential:water_source"] = { chance = 1.0 },
+    ["void_essential:river_water_source"] = { chance = 1.0 },
 }
